79557b5b7ed212ab8ddb4de79d18069b860ee7db
nipun221 <nipunvats5@gmail.com>
2025-12-06 13:11:40 +0530

    fixed deployment step in deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = '/my-devops-portfolio/'; 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: repoName,
   test: {
     globals: true,
     environment: 'jsdom',
